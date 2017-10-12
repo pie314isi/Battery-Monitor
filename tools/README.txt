cd17bb9667a5c9e1c86ee5b8aa469ca57626a0fd
Cory Seligman <coryms@seligman-family.net>
2017-10-12 13:51:16 +1100

    updates for separate router to handle wifi - raspi needs static IP 192.168.1.192

--- tools/README.txt
+++ tools/README.txt
@@ -1,3 +1,5 @@
+
+
 https://wiki.wemos.cc/products:d1:d1_mini_lite
 
 https://forum.wemos.cc/topic/274/how-to-flash-the-new-d1-mini-lite-with-arduino/2
@@ -7,6 +9,8 @@ https://petr.io/en/blog/2015/11/09/read-only-raspberry-pi-with-jessie/
 
 need to use the same method to make /var/lib/misc writeable on /tmp for dnsmasq to assign dhcp leases.
 
+make raspi be an access point - use it for internal admin. separate router is for hosting
+wifi modules
 https://frillip.com/using-your-raspberry-pi-3-as-a-wifi-access-point-with-hostapd/
 
 http://lists.thekelleys.org.uk/pipermail/dnsmasq-discuss/2010q3/004384.html
@@ -15,6 +19,8 @@ http://docs.aws.amazon.com/cli/latest/userguide/awscli-install-linux.html
 
 apt-get install python-pip
 http://docs.aws.amazon.com/cli/latest/userguide/awscli-install-linux.html
+
+configure logrotate to do hourly updates
 
 receive udp packets
 socat UDP-RECV:4120 STDOUT | cat > udp.txt &
--- wifi_module/wifi_module.ino
+++ wifi_module/wifi_module.ino
@@ -10,8 +10,9 @@ extern "C" {
 #define BUTTON      D0
 #define nOLED       D1
 #define RLED        D2
-#define nISENSOR    D8
+#define nISENSOR    D3 // the resistor provide for on D8 already has a pull down, so we'll use D3 and a link under the board
 #define BUILTINLED  D4
+
 
 
 #include <WiFiUdp.h>
@@ -23,7 +24,7 @@ const char* ssid = "Mornington";
 const char* password = "Mornington";
 
 WiFiUDP Udp;
-//IPAddress ip(192, 168, 1, 145);
+IPAddress serverip(192, 168, 1, 192);
 unsigned int localUdpPort = 4210;  // local port to listen on
 char incomingPacket[255];  // buffer for incoming packets
 char  periodicPacket[80];  // a reply string to send back
@@ -34,17 +35,20 @@ char macstr[20];
 
 unsigned int counter=0;
 
+unsigned int quiettimer=0;
 
 void setup()
 {
   pinMode(BUTTON, INPUT_PULLUP);
   pinMode(BUILTINLED, OUTPUT);
 
+  pinMode(nISENSOR, INPUT_PULLUP);
+
   digitalWrite(BUTTON, HIGH);
   digitalWrite(BUILTINLED,HIGH);
   Serial.begin(115200);
-  Serial.println();
-  //Serial.printf("UPGRADED!!!");
+
+    
   Serial.printf("Connecting to %s ", ssid);
   WiFi.mode(WIFI_STA);
   //wifi_set_sleep_type(LIGHT_SLEEP_T);
@@ -52,7 +56,7 @@ void setup()
   
   WiFi.macAddress(mac);
   
-  sprintf(macstr, "%x:%x:%x:%x:%x:%x", mac[0], mac[1], mac[2], mac[3], mac[4], mac[5]);
+  sprintf(macstr, "%02X:%02X:%02X:%02X:%02X:%02X", mac[0], mac[1], mac[2], mac[3], mac[4], mac[5]);
   
   while (WiFi.status() != WL_CONNECTED)
   {
@@ -60,9 +64,18 @@ void setup()
     delay(500);
     Serial.print(".");
   }
+  
   Serial.println(" connected");
 
   Udp.begin(localUdpPort);
+  Serial.printf("Now listening at IP %s, UDP port %d\n", WiFi.localIP().toString().c_str(), localUdpPort);
+  
+  Udp.beginPacket(serverip, 4120);  
+  
+  sprintf(periodicPacket, "%s, BOOT IPADDRESS %s\n",macstr, WiFi.localIP().toString().c_str() );
+  Udp.write(periodicPacket);
+  Udp.endPacket();
+  
   Serial.printf("Now listening at IP %s, UDP port %d\n", WiFi.localIP().toString().c_str(), localUdpPort);
 
   // at boot time, each device should do a http get to read its operating paramters. eg. update rate, upd ports, maybe others
@@ -71,66 +84,138 @@ void setup()
 
 void loop()
 {
-  int packetSize = Udp.parsePacket();
-  if (packetSize)
-  {
-    // receive incoming UDP packets
-    Serial.printf("Received %d bytes from %s, port %d\n", packetSize, Udp.remoteIP().toString().c_str(), Udp.remotePort());
-    int len = Udp.read(incomingPacket, 255);
-    if (len > 0)
-       incomingPacket[len] = 0;
-    Serial.printf("UDP packet contents: %s\n", incomingPacket);
-
-    switch(incomingPacket[0])
+    
+  if (WiFi.status() != WL_CONNECTED)
+    {
+    while (WiFi.status() != WL_CONNECTED)
       {
-        case 'U':
-          ESPhttpUpdate.update(WiFi.gatewayIP().toString(), 8080, "/arduino.bin");
-          // maybe parameters of the upgrade command could specify server and filename to update from
-          break;
-
-        case 'S':
-          // synchronise ADC sampling
-          break;
-
-        case 'F':
-          for(int i=0; i<10; i++) {
-              digitalWrite(BUILTINLED,!digitalRead(BUILTINLED));
-              delay(100);
-              }
-          // flash LED
-          break;
-        
+      digitalWrite(BUILTINLED,!digitalRead(BUILTINLED));
+      delay(500);
+      Serial.print(".");
       }
-    // send back a reply, to the IP address and port we got the packet from
-    //Udp.beginPacket(Udp.remoteIP(), Udp.remotePort());
-    //Udp.write(replyPacket);
-    //Udp.endPacket();
-
-  }
+      Udp.begin(localUdpPort);
+      Serial.printf("Now listening at IP %s, UDP port %d\n", WiFi.localIP().toString().c_str(), localUdpPort);
+      
+      Udp.beginPacket(serverip, 4120);  
+      
+      sprintf(periodicPacket, "%s, CONNECTION REESTABLISHED IPADDRESS %s\n",macstr, WiFi.localIP().toString().c_str() );
+      Udp.write(periodicPacket);
+      Udp.endPacket();
   
-    Udp.beginPacket(WiFi.gatewayIP(), 4120);
+    }
+    IPAddress localip = WiFi.localIP();
+    
     counter++;
-    for(int i=0; i<9; i++)
+    Serial.printf("Main loop counter: %d\n", counter);   
+
+    for(int i=0; i<99; i++)
         {
         delay(100);
-        if(0)//!digitalRead(BUTTON)) 
+        if(!digitalRead(BUTTON)) 
           {
+          Udp.beginPacket(serverip, 4120);  
           Serial.printf("press\n"); 
-          sprintf(periodicPacket, "%s BUTTONPRESS\n",macstr);
+          sprintf(periodicPacket, "%s BUTTONPRESS IPADDRESS %s\n",macstr, WiFi.localIP().toString().c_str() );
           Udp.write(periodicPacket);
           Udp.endPacket();
           }
+          
+          int packetSize = Udp.parsePacket();
+          if (packetSize)
+          {
+            // receive incoming UDP packets
+            Serial.printf("Received %d bytes from %s, port %d\n", packetSize, Udp.remoteIP().toString().c_str(), Udp.remotePort());
+            int len = Udp.read(incomingPacket, 255);
+            if (len > 0)
+               incomingPacket[len] = 0;
+            Serial.printf("UDP packet contents: %s\n", incomingPacket);
+        
+            switch(incomingPacket[0])
+              {
+                case 'U':
+                  ESPhttpUpdate.update(serverip.toString(), 8080, "/arduino.bin");
+                  // ESPhttpUpdate.update(WiFi.gatewayIP().toString(), 8080, "/arduino.bin");
+                  // maybe parameters of the upgrade command could specify server and filename to update from
+                  break;
+                
+                case 'R':
+                  ESP.reset();
+                  break;
+
+                case 'r':
+                  Serial.printf("delay %d seconds\n", localip[3]);   
+                  delay(localip[3]*1000);
+                  Serial.printf("reset\n");   
+                  ESP.reset();
+                  break;
+
+        
+                case 'Q':
+                  quiettimer = counter + 60;
+                  break;
+                
+                case 'q':
+                  quiettimer = 0;
+                  break;
+        
+                case 'F':
+                  for(int i=0; i<10; i++) {
+                      digitalWrite(BUILTINLED,!digitalRead(BUILTINLED));
+                      delay(100);
+                      }
+                  // flash LED
+                  break;
+        
+                case 'I':
+                  Udp.beginPacket(serverip, 4120);  
+                  
+                  sprintf(periodicPacket, "%s, IPADDRESS %s\n",macstr, WiFi.localIP().toString().c_str() );
+                  Udp.write(periodicPacket);
+                  Udp.endPacket();
+        
+                  break;
+
+               case 'V':
+                  Udp.beginPacket(serverip, 4120);  
+                  
+                  sprintf(periodicPacket, "%s, BUILT %s\n",macstr, __DATE__ );
+                  Udp.write(periodicPacket);
+                  Udp.endPacket();    
+
+                  break;                  
+                
+              }
+            // send back a reply, to the IP address and port we got the packet from
+            //Udp.beginPacket(Udp.remoteIP(), Udp.remotePort());
+            //Udp.write(replyPacket);
+            //Udp.endPacket();
+        
+          }
+        
+          
         }
    
     //Serial.printf("talking to : %s\n", Udp.remoteIP().toString().c_str());
-    digitalWrite(BUILTINLED,LOW);
-    delay(100);
-    digitalWrite(BUILTINLED,HIGH);
-    
-    long rssi = WiFi.RSSI();
-    sprintf(periodicPacket, "%s, %d, %d, %d, %d, %d, %d\n",macstr, rssi, counter, (int) (analogRead(A0)), digitalRead(D1), digitalRead(D2), digitalRead(D8));
-    Udp.write(periodicPacket);
-    Udp.endPacket();
+    if(quiettimer==0)
+      {
+      digitalWrite(BUILTINLED,LOW);
+      delay(100);
+      digitalWrite(BUILTINLED,HIGH);
+  
+      Udp.beginPacket(serverip, 4120);
+      long rssi = WiFi.RSSI();
+      
+      sprintf(periodicPacket, "%s, %d, %d, %d, %d, %d, %d\n",macstr, rssi, counter, (int) (analogRead(A0)), digitalRead(D1), digitalRead(D2), digitalRead(nISENSOR));
+      Udp.write(periodicPacket);
+      Udp.endPacket();
+      }
+    else 
+      {
+      Serial.printf("being quiet until: %d\n", quiettimer);   
+      if(counter > quiettimer)
+        quiettimer=0;
+      }      
+      
     yield();
   
 }
